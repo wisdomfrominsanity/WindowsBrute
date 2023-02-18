00e011a8baa2fe71826b2c18f9b05fab4422790d
Blake-Michael Powers <87630933+wisdomfrominsanity@users.noreply.github.com>
2023-02-17 22:23:07 -0800

    Update animal.py

--- animal.py
+++ animal.py
@@ -54,13 +54,11 @@ def animal():
                 guesses += 1
 
         if cracked:
-            print("PASSWORD FOUND: {cracked_password}")
-            subprocess.call("cd " + directory, shell=True
+            print("PASSWORD FOUND:
             subprocess.call("sudo rm -f targethashes.txt", shell=True)
             
         else:
             print("NO PASSWORD FOUND OUT OF {guesses} GUESSES")
-            subprocess.call("cd " + directory, shell=True)
             subprocess.call("sudo rm -f targethashes.txt", shell=True)
             subprocess.call("sudo umount windows_harddrive", shell=True) 
 
